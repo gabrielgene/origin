2f67f08a2b15ba07b0ea8d4e373bcb06b8e5709a
Gabriel Gene <gabrielgene97@gmail.com>
2022-12-08 06:56:08 -0300

    feat: add initial config

---
+++ jest.setup.js
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom/extend-expect';
+
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+
+jest.useFakeTimers().setSystemTime(new Date('2022-12-07'));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
